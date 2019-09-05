3839190123d990f0fd3e0801c1f29be8d38993de
Ali-Khan5 <ali_allrounder@hotmail.com>
2019-09-05 16:08:36 +0500

    initial hovering...

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import "./App.css";
 function App() {
   return (
     <div className="container-fluid text-center">
-      <div className="container" style={{marginTop:'20%'}}>
-      <div className="row" style={{border:'2px solid black'}}>
-      <div className="col-12">
-      <h2 className="text-center"> welcome to task 2</h2>
-        <AvatarComponent/>
-         </div>
+      <div className="container" style={{ marginTop: "20%" }}>
+        <div className="row" style={{ border: "2px solid black" }}>
+          <div className="col-12">
+            <h2 className="text-center"> welcome to task 2</h2>
+            <AvatarComponent />
+          </div>
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 }
--- src/mainComponents/avatar.js
+++ src/mainComponents/avatar.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import './style.css'
+import React, { Component } from "react";
+import "./style.css";
 
-function Avatar() {
-  return (
-    <div className="container-fluid">
-
-    <div className="row">
-        <div className="col-6">
-
-        <img className="avatarImg" src="https://i1.wp.com/frenky.id/wp-content/uploads/2018/02/doge-icon.png?ssl=1"/>
-        </div>
-        <div className="col-6">
+class Avatar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        hovered:false
+    };
+  }
+  setHover = () => this.setState({ hovered: true });
+  cancelHover = () => this.setState({ hovered: false });
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <div className="mainAvatarCard">
+              <div
+                className="avatarcard"
+                onMouseOver={this.setHover}
+                onMouseOut={this.cancelHover}
+              >
+                <img
+                  className="avatarImg align-top float-left"
+                  src="https://i1.wp.com/frenky.id/wp-content/uploads/2018/02/doge-icon.png?ssl=1"
+                />
+                <h4 className="textMiddle float-left"> Mr. Doge Wowzer</h4>
+                <span className="textMiddle float-left online"></span>
+              </div>
+              {this.state.hovered ? <div className="hoverCard ">
+                <h2 className="btn btn-warning float-left">hello world</h2>
+              </div>:null }
+              
             </div>
+          </div>
         </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default Avatar;
